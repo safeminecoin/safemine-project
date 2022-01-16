2259c104ede50c70be246e69704c16bf6d2d2c16
alfariqi <safeminecoin@gmail.com>
2022-01-16 09:06:32 +0700

    Initial Commit

---
+++ forms/notify.php
@@ -0,0 +1,30 @@
+<?php
+
+  $receiving_email_address = 'safeminecoin@gmail.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = "Subscriber";
+  $contact->from_email = $_POST['email'];
+  $contact->subject ="Notify me request";
+
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
+
+  $contact->add_message( $_POST['email'], 'Email');
+
+  echo $contact->send();
+?>
